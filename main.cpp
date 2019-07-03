2221891fc41bd8002f416bd7ed7ee1c25e467b87
Sushain Razdan <razz.sushain@gmail.com>
2019-07-03 19:38:56 +0530

    Add files via upload
    Adding files for Djiktra's shortest path

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdint>
+#include <random>
+
+using namespace std;
+
+const int vertices = 50;
+const double edgeDensity = 0.19;
+
+class Graph {
+private:
+    int vertices;
+    double edgeDensity;
+    int **graph;
+public:
+    Graph(int vertices = 0, double edgeDensity = 0.0, int **graph = nullptr)
+    {
+        this->vertices = vertices;
+        this->edgeDensity = edgeDensity;
+
+        random_device rd; // access hardware RNG
+        default_random_engine e(rd()); // seed the software PRNG
+        uniform_int_distribution<int> cost(1, 10); // edge
+        uniform_real_distribution<double> prob(0.0, 1.0); // range
+
+        graph = new int*[vertices];
+        for (int i = 0; i < vertices; i++)
+            graph[i] = new int[vertices];
+
+        for (int i = 0; i < this->vertices; i++) {
+            for (int j = 0; j < this->vertices; j++) {
+                if (i == j) graph[i][j] = 0;
+                else if (prob(e) < this->edgeDensity) {
+                    graph[i][j] = graph[j][i] = cost(e);
+                }
+                cout<<graph[i][j]<<' ';
+            }
+            cout<<endl;
+        }
+        //display_graph();
+    }
+    void display_graph()
+    {
+        for (int i = 0; i < this->vertices; i++) {
+            for (int j = 0; j < this->vertices; j++) {
+                cout<<graph[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(void)
+{
+    Graph G{vertices, edgeDensity};
+
+    return 0;
+}
+
