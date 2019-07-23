429229f5137fb8b8ad982f1179da2959a1efb254
Sushain Razdan <razz.sushain@gmail.com>
2019-07-23 14:43:15 +0530

    Update main.cpp
    Modifying main file to calculate shortest path

--- main.cpp
+++ main.cpp
@@ -1,129 +1,39 @@
 //============================================================================
-// Name        : Djiktra.cpp
+// Name        : main.cpp
 // Author      : Sushain Razdan
 // Version     :
 // Copyright   :
-// Description : To calculate the average shortest path in a given graph using
-//               Djiktra's shortest path algorithm
+// Description : Generates a graph and calculates average shortest path cost using djikstra's algorithm
 //============================================================================
-#include <iostream>
-#include <cstdlib>
-#include <cstdint>
-#include <random>
+
+#include "Graph.h"
+#include "PriorityQueue.h"
+#include "ShortestPath.h"
 
 using namespace std;
 
-const int vertices = 3;
-const double edgeDensity = 0.19;
-
-class Graph {
-private:
-    int vertices;
-    double edgeDensity;
-    int **graph;
-public:
-    //Constructor allocates a graph based on no of vertices and edge density
-    //edges are randomly created based on the edge density
-    Graph(int vertices = 0, double edgeDensity = 0.0)
-    {
-        this->vertices = vertices;
-        this->edgeDensity = edgeDensity;
-
-        random_device rd; // access hardware RNG
-        default_random_engine e(rd()); // seed the software PRNG
-        uniform_int_distribution<int> cost(1, 10); // edge
-        uniform_real_distribution<double> prob(0.0, 1.0); // range
-
-        graph = new int*[vertices];
-        for (int i = 0; i < vertices; i++) {
-            graph[i] = new int[vertices];
-        }
-
-        for (int i = 0; i < this->vertices; i++) {
-            for (int j = 0; j < this->vertices; j++) {
-                if (i == j) graph[i][j] = 0;
-                else if (prob(e) < this->edgeDensity)
-                    graph[i][j] = graph[j][i] = cost(e);
-                else
-                    graph[i][j] = graph[j][i] = 0;
-            }
-        }
-        DisplayGraph();
-    }
-    void DisplayGraph()
-    {
-        for (int i = 0; i < vertices; i++) {
-            for (int j = 0; j < vertices; j++) {
-                cout<<graph[i][j]<<' ';
-            }
-            cout<<endl;
-        }
-    }
-    int Vertices()
-    {
-       return vertices;
-    }
-    int Edges()
-    {
-        int edgeCounter = 0;
-        for (int i = 0; i < vertices; i++) {
-            for (int j = i; j < vertices; j++) {
-                if (graph[i][j] != 0) edgeCounter++;
-            }
-        }
-        return edgeCounter;
-    }
-    //Find if a connection exists between source and destination
-    bool Adjacent(int source, int dest)
-    {
-        return (graph[source][dest] != 0);
-    }
-    //Find all neighbours of a vertex
-    vector <int> Neighbours(int vertex)
-    {
-        vector <int> neighbours;    //vector taken since neighbours can be variable
-        for (int j = 0; j < vertices; j++){
-            if (graph[vertex][j] != 0) {   //find and push the neighbours
-                neighbours.push_back(j);
-            }
-        }
-        return neighbours;
-    }
-    //Add an Edge if between source and destination if it is unconnected
-    bool AddEdge(int source, int dest, int cost)
-    {
-        if (graph[source][dest] != 0) return false;
-        //Since graph is undirected we assign cost at 2 locations
-        graph[source][dest] = graph[dest][source] = cost;
-        return true;
-    }
-    //Removes an edge between source and destination
-    void RemoveEdge(int source, int dest)
-    {
-        graph[source][dest] = graph[dest][source] = 0;
-    }
-    //Change the value of the edge between source and destination
-    void SetEdgeValue(int source, int dest, int cost)
-    {
-        graph[source][dest] = graph[dest][source] = cost;
-    }
-    //Get the Edge value between source and destination
-    int GetEdgeValue(int source, int dest)
-    {
-        return graph[source][dest];
-    }
-    //Destructor to deallocate resources
-    ~Graph()
-    {
-        for (int i = 0; i < vertices; i++)
-            delete graph[i];
-        delete graph;
-    }
-};
-
 int main(void)
 {
-    Graph G{vertices, edgeDensity};
+    const int vertices = 50;
+    const double edgeDensity[] = {0.20, 0.40};
+    double avgPathCost;
+    ShortestPath sp;
 
+    for (unsigned int i = 0; i < 2; i++) {
+        int sum = 0;
+        int pCount = 0;
+        Graph G(vertices, edgeDensity[i]);
+
+        for (unsigned int j = 1; j < vertices; j++) {
+            int pathsize = sp.pathSize(G, 0, j);
+            if (pathsize != MAX_VALUE) {
+                sum += pathsize;
+                pCount++;
+            }
+        }
+        avgPathCost = static_cast<double>(sum)/pCount;
+        cout << "Graph Density:" << edgeDensity[i] <<" ";
+        cout << "Average Path Length:" << avgPathCost << endl;
+    }
     return 0;
 }
