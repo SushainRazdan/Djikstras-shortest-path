d9af3a48b8ddc9aac6903014676c4801b0794558
Sushain Razdan <razz.sushain@gmail.com>
2019-07-23 14:44:45 +0530

    Update PriorityQueue.cpp
    Modifying PriorityQueue

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -4,83 +4,69 @@
 // Version     :
 // Copyright   :
 // Description : Priority Queue to obtain the min Vertex, Distance pair
-//============================================================================
-#include <iostream>
-#include <cstdlib>
-#include <cstdint>
-#include <vector>
+//===========================================================================
+
+#include "PriorityQueue.h"
 #include <algorithm>
 
 using namespace std;
 
-class QNode {
-
-private:
-    int vertex;
-    int distance;
-
-public:
-    QNode(int vertex = -1, int distance = -1){
-        this->vertex = vertex;
-        this->distance = distance;
-    }
-    int getDistance(void) const { return distance;}
-};
-
-class QComparator
-{
-public:
-    int operator() (const QNode& node1, const QNode& node2)
-    {
-        return node1.getDistance() > node2.getDistance();
-    }
-};
-
-struct QElement {
-    int vertex;
-    int distance;
-};
-
-// Compares two intervals according to staring times.
+// Compares two Elements according to distances.
 bool compareDistance(QElement q1, QElement q2)
 {
     return (q1.distance < q2.distance);
 }
 
+PriorityQueue::PriorityQueue(const vector <int> &vertices)
+{
+    for (unsigned int i = 0; i < vertices.size(); i++) {
+        insertElem(QElement(vertices[i]));
+    }
+}
 
-class PriorityQueue {
+void PriorityQueue::sortQ(void)
+{
+    sort(pq.begin(), pq.end(), compareDistance);
+}
 
-private:
-    //priority_queue <QNode, vector<QNode>, QComparator> pq;
-    vector <QElement> pq;
-public:
-    PriorityQueue(){}
-    void sortQ(void)
-    {
-        sort(pq.begin(), pq.end(), compareDistance);
-    }
-    void changePriority(const QElement &QElem)
-    {
-        for (auto iter = pq.begin(); iter != pq.end(); iter++){
-            if (QElem.vertex == (*iter).vertex) {
-                (*iter).distance = QElem.distance;
-            }
+void PriorityQueue::changePriority(const QElement &QElem)
+{
+    for (auto iter = pq.begin(); iter != pq.end(); iter++){
+        if (QElem.vertex == (*iter).vertex) {
+            (*iter).distance = QElem.distance;
         }
-        sortQ();
     }
-    void minPrioirty()
-    {
-        pq.erase(pq.begin());
-    }
-    bool contains(const QElement &QElem)
-    {
-        for (auto iter = pq.begin(); iter != pq.end(); iter++) {
-            if (QElem.vertex == (*iter).vertex) return true;
-        }
-        return false;
-    }
-    void insertElem(const QElement &QElem)
-    {
-        pq.push_back(QElem);
-    }
-};
+    sortQ();
+}
+
+void PriorityQueue::pop(void)
+{
+    pq.erase(pq.begin());
+}
+
+bool PriorityQueue::contains(QElement &QElem)
+{
+	for (auto iter = pq.begin(); iter != pq.end(); iter++) {
+		if (QElem.vertex == (*iter).vertex) {
+            QElem.distance = (*iter).distance;
+            return true;
+		}
+	}
+	return false;
+}
+
+void PriorityQueue::insertElem(const QElement &QElem)
+{
+	pq.push_back(QElem);
+	sortQ();
+}
+
+QElement PriorityQueue::top(void)
+{
+	return pq.front();
+}
+
+int PriorityQueue::qSize(void)
+{
+	return pq.size();
+}
